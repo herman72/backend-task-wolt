75b5b279c76959864894ec1ea8eb8dfeb2739603
herman72 <m.sherafati7@gmail.com>
2025-01-17 22:03:05 +0200

    :hammer: use pydantic for type check

--- python_approach/app/routers/delivery.py
+++ python_approach/app/routers/delivery.py
@@ -1,27 +1,24 @@
-from fastapi import APIRouter, HTTPException, Query
+from fastapi import APIRouter, HTTPException
 from app.services.venue_service import fetch_venue_data
 from app.utils.calculate_distance import calculate_distance
+from python_approach.app.schemas.models import DeliveryRequest, DeliveryResponse
 
 router = APIRouter()
 
 @router.get("/delivery-order-price", tags=["Delivery"])
-async def delivery_order_price_calculator(
-    venue_slug: str = Query(..., description="Slug of the venue"),
-    cart_value: int = Query(..., gt=0, description="Value of the cart in cents or local currency"),
-    user_lat: float = Query(..., description="Latitude of the user"),
-    user_lon: float = Query(..., description="Longitude of the user"),
-):
+async def delivery_order_price_calculator(request_data: DeliveryRequest):
     
     try:
-        static_data = await fetch_venue_data(venue_slug, "static")
-        dynamic_data = await fetch_venue_data(venue_slug, "dynamic")
+        
+        static_data = await fetch_venue_data(request_data.venue_slug, "static")
+        dynamic_data = await fetch_venue_data(request_data.venue_slug, "dynamic")
 
         venue_coords = static_data["venue_raw"]["location"]["coordinates"]
         venue_lon, venue_lat = venue_coords
-        delivery_distance = calculate_distance(venue_lon, venue_lat, user_lon, user_lat)
+        delivery_distance = calculate_distance(venue_lon, venue_lat, request_data.user_lon, request_data.user_lat)
 
         min_order_value = dynamic_data["venue_raw"]["delivery_specs"]["order_minimum_no_surcharge"]
-        small_order_surcharge = max(0, min_order_value - cart_value)
+        small_order_surcharge = max(0, min_order_value - request_data.cart_value)
 
         base_price = dynamic_data["venue_raw"]["delivery_specs"]["delivery_pricing"]["base_price"]
         distance_ranges = dynamic_data["venue_raw"]["delivery_specs"]["delivery_pricing"]["distance_ranges"]
@@ -35,17 +32,17 @@ async def delivery_order_price_calculator(
         if delivery_fee is None:
             raise HTTPException(status_code=400, detail="Delivery distance is too long")
 
-        total_price = cart_value + small_order_surcharge + delivery_fee
+        total_price = request_data.cart_value + small_order_surcharge + delivery_fee
+        
+        # Prepare the response
+        response = DeliveryResponse(
+            total_price=total_price,
+            small_order_surcharge=small_order_surcharge,
+            cart_value=request_data.cart_value,
+            delivery={"fee": delivery_fee, "distance": round(delivery_distance)}
+        )
 
-        return {
-            "total_price": total_price,
-            "small_order_surcharge": small_order_surcharge,
-            "cart_value": cart_value,
-            "delivery": {
-                "fee": delivery_fee,
-                "distance": round(delivery_distance),
-            },
-        }
+        return response
 
     except HTTPException as e:
         raise e
