c2e2719bf5586bda07c7fa1fcbe1008a49335925
herman72 <m.sherafati7@gmail.com>
2025-01-17 21:06:19 +0200

    :construction: chnage dir of distance calculater handler function to new one

---
+++ python_approach/app/utils/calculate_distance.py
@@ -0,0 +1,15 @@
+import math
+
+def calculate_distance(lon1, lat1, lon2, lat2):
+    
+    lat1, lon1, lat2, lon2 = map(math.radians, [lat1, lon1, lat2, lon2])
+
+    dlat = lat2 - lat1
+    dlon = lon2 - lon1
+    a = math.sin(dlat / 2)**2 + math.cos(lat1) * math.cos(lat2) * math.sin(dlon / 2)**2
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+
+    radius = 6371 * 1000
+    distance = radius * c
+
+    return distance
